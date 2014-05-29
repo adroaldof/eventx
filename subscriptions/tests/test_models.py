6e2a830597284e718de81ce085e2ed6c80023eb9
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-29 11:47:16 -0300

    Add registration tests

---
+++ subscriptions/tests/test_models.py
@@ -0,0 +1,61 @@
+# coding: utf-8
+from datetime import datetime
+from django.test import TestCase
+from django.db import IntegrityError
+
+from subscriptions.models import Subscription
+
+
+class SubscriptionTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Test Testing Name',
+            cpf='12345678901',
+            email='test@testing.com',
+            phone='12-123456789'
+        )
+
+    def test_create(self):
+        'Subscription must have name, cpf, email, phone'
+        self.obj.save()
+        self.assertEqual(1, self.obj.pk)
+
+    def test_has_created_at(self):
+        'Subscription must have a automatic created_at field'
+        self.obj.save()
+        self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_unicode(self):
+        'Verify subscriber name'
+        self.assertEqual(u'Test Testing Name', unicode(self.obj))
+
+
+class SubscriptionUniqueTest(TestCase):
+    def setUp(self):
+        # Create a first entry to force collision on cpf unique test
+        Subscription.objects.create(
+            name='Test Testing Name',
+            cpf='12345678901',
+            email='test@testing.com',
+            phone='12-123456789'
+        )
+
+    def test_cpf_unique(self):
+        'CPF field must be unique'
+        subsc = Subscription(
+            name='Test Testing Same CPF',
+            cpf='12345678901',
+            email='test.other@testing.com',
+            phone='12-123456789'
+        )
+        self.assertRaises(IntegrityError, subsc.save)
+
+    def test_email_unique(self):
+        'Email field must be unique'
+        subsc = Subscription(
+            name='Test Testing Same Email',
+            cpf='88888888888',
+            email='test@testing.com',
+            phone='12-123456789'
+        )
+        self.assertRaises(IntegrityError, subsc.save)
---
+++ subscriptions/tests/test_views_subscribe.py
@@ -0,0 +1,84 @@
+from django.test import TestCase
+
+from subscriptions.forms import SubscriptionForm
+from subscriptions.models import Subscription
+
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/inscricao/')
+
+    def test_get(self):
+        'GET /inscricao/ must return status code 200'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_template(self):
+        'Response should be a rendered subscriptions/subscription_form.html'
+        self.assertTemplateUsed(
+            self.resp, 'subscriptions/subscription_form.html'
+        )
+
+    # This can be done with PyQuery
+    def test_html(self):
+        'HTML must contain 3 text, 1 email, 1 hidden and 1 submit controls'
+        self.assertContains(self.resp, '<form')
+        self.assertContains(self.resp, '<input', 6)
+        self.assertContains(self.resp, 'type="text"', 4)
+        self.assertContains(self.resp, 'type="email"')
+        self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        'HTML must contain csrf token'
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        'Context must have the subscription form'
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        'Form must have four fields'
+        form = self.resp.context['form']
+        self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
+
+
+class SubscribePostTest(TestCase):
+    def setUp(self):
+        data = dict(
+            name='Test Testing Name',
+            cpf='12345678901',
+            email='test@testing.com',
+            phone='12-123456789'
+        )
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        'Valid POST should redirect to /incricao/1/'
+        self.assertEqual(302, self.resp.status_code)
+
+    def test_save(self):
+        'Valid POST must be saved'
+        self.assertTrue(Subscription.objects.exists())
+
+
+class SubscribeInvalidPostTest(TestCase):
+    def setUp(self):
+        data = dict(
+            name='Test Testing Name',
+            cpf='123456789012',
+            email='test@testing.com',
+            phone='12-123456789'
+        )
+        self.resp = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        'Inalid POST should not redirect'
+        self.assertEqual(200, self.resp.status_code)
+
+    def test_form_errors(self):
+        'Form must contain errors'
+        self.assertTrue(self.resp.context['form'].errors)
+
+    def test_dont_save(self):
+        'Do not save data'
+        self.assertFalse(Subscription.objects.exists())
