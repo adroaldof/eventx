665e5ac99e301b1a4253b61e2e1023f48928cbdb
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-29 11:48:12 -0300

    Add registration views, forms and models

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Subscription(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    cpf = models.CharField(_('CPF'), max_length=11, unique=True)
+    email = models.EmailField(_('email'), unique=True)
+    phone = models.CharField(_('phone'), max_length=20, blank=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
+        verbose_name = _('Subscription')
+        verbose_name_plural = _('Subscriptions')
+
+    def __unicode__(self):
+        return self.name
