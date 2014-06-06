35fba1aeb015b09180e6d6a1d22e927d4d47b7c8
Adroaldo de Andrade <adroaldof@gmail.com>
2014-06-06 18:42:38 -0300

    Make CPF validation

--- subscriptions/forms.py
+++ subscriptions/forms.py
@@ -1,9 +1,24 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
 
 from .models import Subscription
+
+
+def CPFValidator(value):
+    if not value.isdigit():
+        raise ValidationError(_('CPF must have only digits'))
+
+    if len(value) != 11:
+        raise ValidationError(_('CPF must have eleven digits'))
 
 
 class SubscriptionForm(forms.ModelForm):
     class Meta:
         model = Subscription
         exclude = ('paid',)
+
+    def __init__(self, *args, **kwargs):
+        super(SubscriptionForm, self).__init__(*args, **kwargs)
+
+        self.fields['cpf'].validators.append(CPFValidator)
--- subscriptions/tests/test_forms.py
+++ subscriptions/tests/test_forms.py
@@ -8,3 +8,25 @@ class SubscriptionFormTest(TestCase):
         'Form must have four fields'
         form = SubscriptionForm()
         self.assertItemsEqual(['name', 'email', 'cpf', 'phone'], form.fields)
+
+    def test_cpf_is_digit(self):
+        'CPF must accept only digits'
+        form = self.make_validated_form(cpf='AAAAA678901')
+        self.assertItemsEqual(['cpf'], form.errors)
+
+    def test_cpf_has_eleven_digits(self):
+        'CPF must have eleven digits'
+        form = self.make_validated_form(cpf='12345')
+        self.assertItemsEqual(['cpf'], form.errors)
+
+    def make_validated_form(self, **kwargs):
+        data = dict(
+            name='Test Testing Name',
+            cpf='12345678901',
+            email='test@testing.com',
+            phone='12-123456789'
+        )
+        data.update(kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
