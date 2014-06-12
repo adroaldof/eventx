b91630b98dbcf9d805e84adfff3e0df743fc5dcd
Adroaldo de Andrade <adroaldof@gmail.com>
2014-06-12 01:12:06 -0300

    Add speaker detail view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 # coding: utf-8
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
+
+from core.models import Speaker
 
 
 def home(request):
     return TemplateResponse(request, 'index.html', {})
+
+
+def speaker_detail(request, slug):
+    speaker = get_object_or_404(Speaker, slug=slug)
+    return TemplateResponse(request, 'core/speaker_detail.html', {
+        'speaker': speaker
+    })
