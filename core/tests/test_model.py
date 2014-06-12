e071bb4e582745017fc8ca71be8a7367bdafd7d5
Adroaldo de Andrade <adroaldof@gmail.com>
2014-06-12 18:34:27 -0300

    Add contact model tests

--- core/tests/test_model.py
+++ core/tests/test_model.py
@@ -1,6 +1,8 @@
+# coding: utf-8
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-from core.models import Speaker
+from core.models import Speaker, Contact
 
 
 class SpeakerModelTest(TestCase):
@@ -20,3 +22,54 @@ class SpeakerModelTest(TestCase):
     def test_unicode(self):
         'Speaker string representation should be the name'
         self.assertEqual('Test Testing', unicode(self.speaker))
+
+
+class ContactModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Test Testing',
+            slug='test-testing',
+            url='http://testing.test.com',
+            description='Passionate testing developer!'
+        )
+
+    def test_email(self):
+        'Test contact email'
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind='E',
+            value='test@testing.com'
+        )
+        self.assertEqual(1, contact.pk)
+
+    def test_phone(self):
+        'Test contact phone'
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind='P',
+            value='12-123456789'
+        )
+        self.assertEqual(1, contact.pk)
+
+    def test_fax(self):
+        'Test contact fax'
+        contact = Contact.objects.create(
+            speaker=self.speaker,
+            kind='E',
+            value='12-234567890'
+        )
+        self.assertEqual(1, contact.pk)
+
+    def test_kind(self):
+        'Contact kind should be limited to E, P or F'
+        contact = Contact(speaker=self.speaker, kind='A', value='B')
+        self.assertRaises(ValidationError, contact.full_clean)
+
+    def test_unicode(self):
+        'Contact string representation should be value'
+        contact = Contact(
+            speaker=self.speaker,
+            kind='E',
+            value='test@testing.com'
+        )
+        self.assertEqual('test@testing.com', unicode(contact))
