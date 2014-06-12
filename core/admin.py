bb5da50dac9cd94d86391a0adb1590814fb94bac
Adroaldo de Andrade <adroaldof@gmail.com>
2014-06-12 19:07:07 -0300

    Add Speaker admin and its contacts inlines

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
+# coding: utf-8
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
-# Register your models here.
+from .models import Speaker, Contact
+
+
+class ContactInline(admin.TabularInline):
+    model = Contact
+    extra = 1
+
+
+class SpeakerAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Speaker Info'), {'fields': ('name', 'slug', 'url', )}),
+        (_('Speaker Description'), {'fields': ('description', )}),
+    )
+    list_display = ('name', 'url', )
+    inlines = [ContactInline, ]
+    prepopulated_fields = {'slug': ('name', )}
+
+admin.site.register(Speaker, SpeakerAdmin)
