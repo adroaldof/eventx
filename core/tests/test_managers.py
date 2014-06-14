5cd2d55331d188a494423f57e7e4475bc91db691
Adroaldo de Andrade <adroaldof@gmail.com>
2014-06-14 10:01:27 -0300

    Add a period manager to use on talks

--- core/tests/test_managers.py
+++ core/tests/test_managers.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from django.test import TestCase
 
-from core.models import Contact, Speaker
+from core.models import Contact, Speaker, Talk
 
 
 class ContactManagerTest(TestCase):
@@ -35,3 +35,21 @@ class ContactManagerTest(TestCase):
         qs = Contact.faxes.all()
         expected = ['<Contact: 12-234567890>']
         self.assertQuerysetEqual(qs, expected)
+
+
+class PeriodManagerTest(TestCase):
+    def setUp(self):
+        Talk.objects.create(title='Morning Talk', start_time='10:00')
+        Talk.objects.create(title='Afternoon Talk', start_time='12:00')
+
+    def test_morning(self):
+        'Morning talks should return only talks before 12:00'
+        self.assertQuerysetEqual(
+            Talk.objects.at_morning(), ['Morning Talk'], lambda t: t.title
+        )
+
+    def test_afternoon(self):
+        'Afternoon talks should return only talks after 12:00'
+        self.assertQuerysetEqual(
+            Talk.objects.at_afternoon(), ['Afternoon Talk'], lambda t: t.title
+        )
